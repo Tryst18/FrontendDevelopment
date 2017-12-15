dc6de3eb477156bbc01dcda1646a6d502fcd68e7
akshayneema <neemakshay111@gmail.com>
2017-12-15 15:41:32 +0530

    footer color issue

--- app/js/footer.js
+++ app/js/footer.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $('.foot .hb-button').on('click', function(){
         $('nav.foot ul').toggleClass('show');
+        // $('nav.foot ul li a').toggleClass('colorflip');
     });
 });
