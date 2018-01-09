fc978ae59f768ccdc1f5eaada3a058d6d25cc63c
Varuni <sarwal8@gmail.com>
2018-01-09 21:13:45 +0530

    icons

--- app/js/footer.js
+++ app/js/footer.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $('.foot .hb-button').on('click', function(){
         $('nav.foot ul').toggleClass('show');
-        // $('nav.foot ul li a').toggleClass('colorflip');
+        $('nav.foot ul li a').toggleClass('colorflip');
+        $('.foot .hb-button').css("transform", "rotate(180deg)");
     });
 });
--- app/js/responsive_navbar.js
+++ app/js/responsive_navbar.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-    $('.head .hb-button').on('click', function(){
+    $('.head .hb-button').on('click', function(e){
+    	e.stopPropagation();
         $('nav.head ul').toggleClass('show');
     });
+});
+
+$(document).click(function() {
+	// $(document).on('click', function(){
+ //        $('nav.head ul').toggleClass('show');
+ //    });
 });
