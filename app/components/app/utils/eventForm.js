24ca67fb1acbc8be1c6c46f8d55891f688bcb151
rish-mahe <mt6140561@iitd.ac.in>
2018-01-20 22:20:16 +0530

    events form working

--- app/components/app/utils/eventForm.js
+++ app/components/app/utils/eventForm.js
@@ -37,18 +37,18 @@
     var app$utils$eventForm$$send = {}
 
 
-    var app$utils$eventForm$$mode = document.getElementById('reg_mode');
-    app$utils$eventForm$$mode.addEventListener('click', function() {
-        for (var x in app$utils$eventForm$$mode.children) {
-            if (x<app$utils$eventForm$$mode.children.length) {
-                if (app$utils$eventForm$$mode.children[x].checked) {
-                                    
-                }
-            }
-        }
-    });
+    // var mode = document.getElementById('reg_mode')
+    // mode.addEventListener('click', function() {
+    //     for (var x in mode.children) {
+    //         if (x<mode.children.length) {
+    //             if (mode.children[x].checked) {
 
-    document.getElementById('reg_mode').addEventListener('change', function() {
+    //             }
+    //         }
+    //     }
+    // })
+
+    document.getElementById('reg_type').addEventListener('change', function() {
         if (document.getElementById('mo2').checked) {
             document.getElementById('team').hidden = false
         } else {
@@ -56,7 +56,7 @@
         }
     });
 
-    document.getElementById('reg_type').addEventListener('change', function() {
+    document.getElementById('reg_mode').addEventListener('change', function() {
         if (document.getElementById('ty2').checked) {
             document.getElementById('t3').hidden = false
             document.getElementById('t4').hidden = true
@@ -85,7 +85,7 @@
                     }
                 }
         })
-        for (var x=1; x<=6; x++) {
+        for (var x=1; x<=8; x++) {
             var targ = document.getElementById('t'+x)
             app$utils$eventForm$$send = Object.assign(app$utils$eventForm$$send, {[targ.name]:targ.value})
         }
@@ -107,19 +107,19 @@
         var xhr = new XMLHttpRequest();
         xhr.open("POST", $$index$$url+"/api/user/login", true);
         xhr.setRequestHeader("Content-type", "application/json");
-        document.write('loading...')
+        document.getElementById('message').innerHTML = ('loading...')
         xhr.onreadystatechange = function () {
             var json = JSON.parse(xhr.responseText);
-            
+            document.getElementById('message').innerHTML = xhr.responseText
             // console.log(xhr.responseText + " " + xhr.status, "response");
             if (xhr.readyState === 4){
                 if (xhr.status === 200) {
-                    document.write('email and pasword checked')		
+                    document.getElementById('message').innerHTML = ('email and password checked')		
                     var data = json.data;
                     // console.log(json.error == false)
                     if (json.error == false) {
-                        // sessionStorage.setItem("token", data.token);
-                        // console.log("login")
+                        sessionStorage.setItem("token", data.token);
+                        console.log("login")
                         var xt = new XMLHttpRequest();
                         xt.open("POST", $$index$$url+"/api/event/create", true)
                         xt.setRequestHeader("Content-type", "application/json")
@@ -127,18 +127,20 @@
                         xt.onreadystatechange = function() {
                             if (xt.readyState === 4) {
                                 if (xt.status === 200) {
-                                    document.write('ignore the loading message' + JSON.parse(xt.responseText).message)
+                                    document.getElementById('message').innerHTML = ('cool' + (xt.responseText))
                                 } else {
-                                    document.write('don\'t wait for loading to go, some problem has occured')
+                                    document.getElementById('message').innerHTML = ('some problem has occured')
                                 }
                             }
                         }
-                        xt.send(JSON.stringify(app$utils$eventForm$$send))
+                        console.log(({"event":app$utils$eventForm$$send}))
+                        xt.send(JSON.stringify({"event":app$utils$eventForm$$send}))
                     }
                 }
             }
         }
-        xhr.send(JSON.stringify({"email":document.getElementById('em'), "password":document.getElementById('pa')}))
+        console.log(JSON.stringify({"email":document.getElementById('em').value, "password":document.getElementById('pa').value}))
+        xhr.send(JSON.stringify({"email":document.getElementById('em').value, "password":document.getElementById('pa').value}))
 
     });
 }).call(this);
--- app/utils/eventForm.js
+++ app/utils/eventForm.js
@@ -3,18 +3,18 @@ import {url} from "./index.js"
 var send = {}
 
 
-var mode = document.getElementById('reg_mode')
-mode.addEventListener('click', function() {
-    for (var x in mode.children) {
-        if (x<mode.children.length) {
-            if (mode.children[x].checked) {
+// var mode = document.getElementById('reg_mode')
+// mode.addEventListener('click', function() {
+//     for (var x in mode.children) {
+//         if (x<mode.children.length) {
+//             if (mode.children[x].checked) {
                                 
-            }
-        }
-    }
-})
+//             }
+//         }
+//     }
+// })
 
-document.getElementById('reg_mode').addEventListener('change', function() {
+document.getElementById('reg_type').addEventListener('change', function() {
     if (document.getElementById('mo2').checked) {
         document.getElementById('team').hidden = false
     } else {
@@ -22,7 +22,7 @@ document.getElementById('reg_mode').addEventListener('change', function() {
     }
 })
 
-document.getElementById('reg_type').addEventListener('change', function() {
+document.getElementById('reg_mode').addEventListener('change', function() {
     if (document.getElementById('ty2').checked) {
         document.getElementById('t3').hidden = false
         document.getElementById('t4').hidden = true
@@ -51,7 +51,7 @@ document.getElementById('launch').addEventListener('click', function() {
                 }
             }
     })
-    for (var x=1; x<=6; x++) {
+    for (var x=1; x<=8; x++) {
         var targ = document.getElementById('t'+x)
         send = Object.assign(send, {[targ.name]:targ.value})
     }
@@ -73,19 +73,19 @@ document.getElementById('launch').addEventListener('click', function() {
     var xhr = new XMLHttpRequest();
 	xhr.open("POST", url+"/api/user/login", true);
     xhr.setRequestHeader("Content-type", "application/json");
-    document.write('loading...')
+    document.getElementById('message').innerHTML = ('loading...')
 	xhr.onreadystatechange = function () {
         var json = JSON.parse(xhr.responseText);
-		
+		document.getElementById('message').innerHTML = xhr.responseText
 		// console.log(xhr.responseText + " " + xhr.status, "response");
 	    if (xhr.readyState === 4){
 			if (xhr.status === 200) {
-				document.write('email and pasword checked')		
+				document.getElementById('message').innerHTML = ('email and password checked')		
 				var data = json.data;
 				// console.log(json.error == false)
 				if (json.error == false) {
-					// sessionStorage.setItem("token", data.token);
-                    // console.log("login")
+					sessionStorage.setItem("token", data.token);
+                    console.log("login")
                     var xt = new XMLHttpRequest();
                     xt.open("POST", url+"/api/event/create", true)
                     xt.setRequestHeader("Content-type", "application/json")
@@ -93,17 +93,19 @@ document.getElementById('launch').addEventListener('click', function() {
                     xt.onreadystatechange = function() {
                         if (xt.readyState === 4) {
                             if (xt.status === 200) {
-                                document.write('ignore the loading message' + JSON.parse(xt.responseText).message)
+                                document.getElementById('message').innerHTML = ('cool' + (xt.responseText))
                             } else {
-                                document.write('don\'t wait for loading to go, some problem has occured')
+                                document.getElementById('message').innerHTML = ('some problem has occured')
                             }
                         }
                     }
-                    xt.send(JSON.stringify(send))
+                    console.log(({"event":send}))
+                    xt.send(JSON.stringify({"event":send}))
 				}
             }
         }
     }
-    xhr.send(JSON.stringify({"email":document.getElementById('em'), "password":document.getElementById('pa')}))
+    console.log(JSON.stringify({"email":document.getElementById('em').value, "password":document.getElementById('pa').value}))
+    xhr.send(JSON.stringify({"email":document.getElementById('em').value, "password":document.getElementById('pa').value}))
 
 })
